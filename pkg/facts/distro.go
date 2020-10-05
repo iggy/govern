e578f27021627aa9e07886c9e60444cc38c55430
iggy <iggy@kws1.com>
2020-10-04 22:23:27 -0700

    Add facts
    This is the code that figures out facts about the local system. We
    still have more to clean up/commit to have something useful.

---
+++ pkg/facts/distro.go
@@ -0,0 +1,100 @@
+// Copyright © 2020 Iggy <iggy@theiggy.com>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package facts
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+type DistroFacts struct {
+	Name       string
+	Slug       string
+	Family     string
+	Version    string
+	Codename   string
+	InitSystem string
+}
+
+// DistroAlpine - return true if we are on alpine distro
+func DistroAlpine() bool {
+	ar, err := ioutil.ReadFile("/etc/alpine-release")
+	if err != nil {
+		return false
+	}
+	Facts.Distro.Name = "Alpine"
+	Facts.Distro.Slug = "alpine"
+	Facts.Distro.Family = "alpine"
+	Facts.Distro.Version = strings.TrimSpace(fmt.Sprintf("%s", ar))
+	// This could technically be different, but never heard of it in practice
+	Facts.Distro.InitSystem = "openrc"
+	return true
+}
+
+// DistroUbuntu - return true if we are on a Ubuntu distro
+func DistroUbuntu() bool {
+	lsb, err := os.Open("/etc/lsb-release")
+
+	switch err.(type) {
+	case *os.PathError:
+		return false
+	default:
+		scanner := bufio.NewScanner(lsb)
+		for scanner.Scan() {
+			line := scanner.Text()
+			if strings.HasPrefix(line, "DISTRIB_RELEASE") {
+				Facts.Distro.Version = fmt.Sprint(strings.Split(line, "=")[1])
+			}
+			if strings.HasPrefix(line, "DISTRIB_CODENAME") {
+				Facts.Distro.Codename = fmt.Sprint(strings.Split(line, "=")[1])
+			}
+		}
+		if err = scanner.Err(); err != nil {
+			log.Panicln(err)
+		}
+		Facts.Distro.Name = "Ubuntu"
+		Facts.Distro.Slug = "ubuntu"
+		Facts.Distro.Family = "debian"
+		// This is the one that's been in use for a while
+		// but it could technically be upstart as well
+		// FIXME actually determine which one is in use
+		Facts.Distro.InitSystem = "systemd"
+		return true
+	}
+}
+
+func init() {
+	DistroAlpine()
+	DistroUbuntu()
+}
---
+++ pkg/facts/hardware.go
@@ -0,0 +1,151 @@
+// Copyright © 2020 Iggy <iggy@theiggy.com>
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+//    may be used to endorse or promote products derived from this software
+//    without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+package facts
+
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/rs/zerolog/log"
+)
+
+// GetSystemUUID - return/fill in the system UUID
+func GetSystemUUID() string {
+	// The system UUID can be in one of a few places
+	// /sys/class/dmi/id/product_uuid
+	// /sys/class/dmi/id/board_serial
+	uuid, err := ioutil.ReadFile("/sys/class/dmi/id/product_uuid")
+	if err != nil {
+		// log.Printf("error getting System UUID: %v\n", err)
+		if os.IsPermission(err) {
+			Facts.SystemUUID = fmt.Sprintf("Unable to open UUID file, are you root? (%s)", err)
+		} else {
+			Facts.SystemUUID = fmt.Sprintf("error: %v", err)
+		}
+	} else {
+		Facts.SystemUUID = strings.TrimSpace(fmt.Sprintf("%s", uuid))
+	}
+	return Facts.SystemUUID
+}
+
+// CPUInfoFacts - info about the CPU(s) in the system
+type CPUInfoFacts struct {
+	Arch    string
+	Vendor  string
+	Model   string
+	Cores   int
+	Threads int
+	Flags   []string
+}
+
+// GetCPUInfo - fill in CPUInfo struct
+func GetCPUInfo() {
+	// arch/machine comes from uname syscall
+	uts := syscall.Utsname{}
+	err := syscall.Uname(&uts)
+	if err != nil {
+		log.Warn().Err(err).Msg("failed to run uname syscall")
+	}
+	// fmt.Printf("uts.Machine: %d\n", uts.Machine)
+	for _, ch := range uts.Machine {
+		Facts.CPUInfo.Arch = fmt.Sprintf("%s%s", Facts.CPUInfo.Arch, string(ch))
+	}
+
+	// model/cores/etc comes from /proc/cpuinfo
+	ci, err := os.Open("/proc/cpuinfo")
+
+	switch err.(type) {
+	case *os.PathError:
+		log.Print("failed to open /proc/cpuinfo")
+	default:
+		scanner := bufio.NewScanner(ci)
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			// vendor ID for x86/x86_64
+			if strings.HasPrefix(line, "vendor_id") {
+				vid := fmt.Sprint(strings.TrimSpace(strings.Split(line, ":")[1]))
+				if vid == "GenuineIntel" {
+					Facts.CPUInfo.Vendor = "intel"
+				}
+				if vid == "AuthenticAMD" {
+					Facts.CPUInfo.Vendor = "amd"
+				}
+
+			}
+			// vendor ID for arm (ish)
+			if strings.HasPrefix(line, "CPU implementor") {
+				ci := fmt.Sprint(strings.TrimSpace(strings.Split(line, ":")[1]))
+				switch ci {
+				case "0x41":
+					// not sure about this
+					// https://github.com/karelzak/util-linux/pull/564/files
+					// says 0x41 is just "arm", but my NAS says realtek
+					Facts.CPUInfo.Vendor = "realtek"
+				case "0x42":
+					Facts.CPUInfo.Vendor = "broadcom"
+				case "0x43":
+					Facts.CPUInfo.Vendor = "cavium"
+
+				}
+
+			}
+
+			if strings.HasPrefix(line, "model name") {
+				Facts.CPUInfo.Model = fmt.Sprint(strings.TrimSpace(strings.Split(line, ":")[1]))
+			}
+			if strings.HasPrefix(line, "cpu cores") {
+				Facts.CPUInfo.Cores, err = strconv.Atoi(strings.TrimSpace(strings.Split(line, ":")[1]))
+				if err != nil {
+					log.Printf("couldn't convert cores: %v\n", err)
+				}
+			}
+			if strings.HasPrefix(line, "siblings") {
+				Facts.CPUInfo.Threads, err = strconv.Atoi(strings.TrimSpace(strings.Split(line, ":")[1]))
+				if err != nil {
+					log.Printf("couldn't convert threads: %v\n", err)
+				}
+			}
+			if strings.HasPrefix(line, "flags") {
+				Facts.CPUInfo.Flags = strings.Split(strings.Split(line, ":")[1], " ")[1:]
+			}
+		}
+	}
+}
+
+func init() {
+	GetSystemUUID()
+	GetCPUInfo()
+}
